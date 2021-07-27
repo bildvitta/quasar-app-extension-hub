d0189647ca1139f3a1ec8c23ca08c1a6ce1cbd1d
Caio Tarifa <caiotarifa@gmail.com>
2021-07-27 14:02:31 -0300

    Releasing v1.2.0

--- src/boot/can.js
+++ src/boot/can.js
@@ -1,15 +1,18 @@
 export default async ({ Vue, store }) => {
   function can (permission, entity) {
     try {
-      const { userPermissions, isSuperuser } = store.getters['hub/user']
+      const { isSuperuser, userPermissions } = store.getters['hub/user']
 
-      if (isSuperuser) return true
+      if (isSuperuser) {
+        return true
+      }
 
       if (Object.prototype.hasOwnProperty.call(userPermissions, permission)) {
-        if (entity) {
-          return userPermissions[permission] === '*' || userPermissions[permission].includes(entity)
-        }
-        return true
+        const userPermission = userPermissions[permission]
+
+        return entity
+          ? (userPermission === '*' || userPermission.includes(entity))
+          : true
       }
     } catch {
       return false
