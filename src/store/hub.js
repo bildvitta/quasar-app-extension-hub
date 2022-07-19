d42643e1cda3fbb530947fa70f7e8a19035748c7
Douglas Calora <douglascalora123@gmail.com>
2022-07-19 13:41:24 -0300

    feat: :sparkles: added feature of getting user before entering the application

--- src/store/hub.js
+++ src/store/hub.js
@@ -46,6 +46,7 @@ const stateData = {
 const getters = {
   accessToken: state => state.accessToken,
   hasAccessToken: state => hasString(state.accessToken),
+  hasUser: state => !!Object.keys(state.user),
   user: state => state.user,
   userPermissions: state => state.user.userPermissions
 }
